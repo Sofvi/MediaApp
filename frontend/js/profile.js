7767f84dabb34be5ca17c93a27e76bbdbe6ab994
Sofvi <laitinen.suvi99@gmail.com>
2022-12-08 19:48:30 +0200

    Added a popup window for adding profile picture

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -2,6 +2,9 @@
 
 const homeBtn = document.querySelector('.fa-solid.fa-house');
 const postBtn = document.querySelector('.fa-solid.fa-square-plus');
+const profilePicBtn = document.querySelector('.profileTopDiv .cardProfileImg');
+const popup = document.getElementById('myPopup');
+const close = document.getElementsByClassName('closePopup');
 
 // Move to home
 homeBtn.addEventListener('click', () => {
@@ -14,3 +17,23 @@ postBtn.addEventListener('click', () => {
   location.href = 'post.html';
   console.log('redirect to post');
 });
+
+// Popup window to add a profile picture
+profilePicBtn.onclick = function() {
+  popup.style.display = 'block';
+}
+
+// Close the popup window
+close.onclick = function() {
+  popup.style.display = 'none';
+  console.log('closing the popup');
+}
+
+// Loading the preview picture
+const loadFile = function(event) {
+  const output = document.getElementById('output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src); // free memory
+  };
+};
