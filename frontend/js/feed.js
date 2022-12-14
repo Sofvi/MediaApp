d723cda12ef5f153d22d567fe72a68325d75dfba
Sofvi <laitinen.suvi99@gmail.com>
2022-12-14 13:06:24 +0200

    Had to copy the files from git because of conflict

--- frontend/js/feed.js
+++ frontend/js/feed.js
@@ -1,186 +1,48 @@
-"use strict";
+'use strict';
 
-const url = "http://localhost:3000"; // change url when uploading to server
-let likeCounter = 0;
-const token = sessionStorage.getItem("token");
+const url = 'http://localhost:3000'; // change url when uploading to server
+
+// select html element
+const ul = document.querySelector('#picList');
+const profileBtn = document.querySelector('.fa-solid.fa-user');
+const postBtn = document.querySelector('.fa-solid.fa-square-plus');
+const likeBtn = document.querySelector('.fa-regular.fa-heart');
+const commentBtn = document.querySelector('.fa-regular.fa-comment');
+const loginBtn = document.querySelector('.fa-right-to-bracket');
+
+const token = sessionStorage.getItem('token');
 const user = sessionStorage.getItem("user");
-const parsedUser = JSON.parse(user);
-console.log(parsedUser);
 
-const getQParam = (param) => {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
+// create cards
+const createFeedCards = (pics) => {
+  pics.forEach((pic) => {
+    const img = document.createElement('img');
+    img.src = url + '/' + pic.filename;
+    img.alt = pic.name;
+    img.classList.add('resp');
 
-  return urlParams.get(param);
-};
+    const figure = document.createElement('figure').appendChild(img);
 
-const feed = document.querySelector(".feed-list");
-const addPost = document.querySelector("#addPost");
-const userLogin = document.querySelector("#user-login");
+    // add like button here //
 
-const profileBtn = document.querySelector(".fa-solid.fa-user");
+    // add comment button here //
 
-const likeBtn = document.querySelector(".fa-regular fa-heart");
-const commentBtn = document.querySelector(".fa-regular.fa-comment");
-const loginBtn = document.querySelector(".fa-right-to-bracket");
+    const  li = document.createElement('li');
+    li.classList.add('light-border');
 
-userLogin.addEventListener("click", () => {
-  if (!user && !token) {
-    alert("You need to log in to post!!");
-    location.href = "../html/login.html";
-  } else {
-    location.href = "../html/feed.html";
-  }
-});
-/* 
-loginBtn.addEventListener("click", () => {
-  location.href = "../html/login.html";
-  console.log("redirect to login");
-});
-// Move to profile
-profileBtn.addEventListener("click", () => {
-  location.href = "../html/profile.html";
-  console.log("redirect to feed");
-});
-
-/* likeBtn.addEventListener("click", () => {
-  if (likeBtn.className === ".fa-regular fa-heart") {
-    likeBtn.className = "fa-solid fa-heart";
-  } else {
-    likeBtn.className = "fa-regular fa-heart";
-    console.log("like clicked");
-  }
-}); 
-*/
-
-addPost.addEventListener("click", () => {
-  if (!user && !token) {
-    alert("You need to log in to post!!");
-    location.href = "../html/login.html";
-  } else {
-    location.href = "../html/post.html";
-  }
-});
-
-const createFeedPost = (posts) => {
-  feed.innerHTML = "";
-  posts.forEach((post) => {
-    console.log(post);
-    likeCounter = post.like_num;
-    feed.innerHTML += `
-     <div class="card">
-  <div class="cardTopDiv">
-    <img
-              src="https://place-puppy.com/400x400"
-              alt="Dog"
-              class="cardProfileImg"
-            />
-            <div class="text-container">
-            <h3>${post.profilename}</h3>
-            <p class="location">${post.location}</p>
-            
-          </div>
-  </div>
-  
-  
-  <img
-      src="${url + "/thumbnails/" + post.filename}"
-      alt="${post.profilename}'s post"
-      style="width: 100%"
-      />
-      </a>
-      <div class="container">
-      <div class="likes-comments">
-      <i  class="fa-regular fa-heart"></i>
-      <i  class="fa-regular fa-comment"></i>
-      </div>
-
-      <p class ="likes">Likes:  ${post.like_num}</>
-      <p class="description">${post.description}</p>
-      
-      </div>
-      <div> <a href="../html/edit-post.html?id=${
-        post.id
-      }"><i class="fa fa-edit" style="color: #aca891"></i> </a></div>
-      </div>`;
-  });
-
-  // if (likeBtn.className === ".fa-regular fa-heart") {
-  //   likeBtn.className = "fa-solid fa-heart";
-  // } else {
-  //   likeBtn.className = "fa-regular fa-heart";
-  // }
-  // const editPost = document.querySelector(".fa.fa-edit");
-  // //console.log(editPost);
-  // editPost.addEventListener("click", () => {
-  //   alert("Redirecting to edit post");
-  //   location.href = "../html/edit-post.html?id=${post.id}";
-  // });
-
-  // editPost.addEventListener("click", () => {
-  //   alert("You are redirected to edit the post");
-  //   location.href = "../edit-post.html";
-  // });
-
-  const likeButton = document.querySelectorAll(".fa-regular.fa-heart");
-  const totalLikes = document.querySelectorAll(".likes");
-
-  likeButton.forEach((like, index) => {
-    like.addEventListener("click", async () => {
-      console.log("clicked", index, posts[index].id);
-      likeButton.className = "fa-solid.fa-heart";
-
-      try {
-        const fetchOptions = {
-          method: "POST",
-          headers: {
-            Authorization: "Bearer " + sessionStorage.getItem("token"),
-          },
-        };
-        const response = await fetch(
-          url + "/post/like/" + posts[index].id,
-          fetchOptions
-        );
-        //console.log(response);
-      } catch (e) {
-        console.log(e.message);
-      }
-    });
-  });
-
-  //*Gets the total number of likes and render*/
-
-  totalLikes.forEach(async (like, index) => {
-    try {
-      const fetchOptions = {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + sessionStorage.getItem("token"),
-        },
-      };
-      const response = await fetch(
-        url + "/post/like/" + posts[index].id,
-        fetchOptions
-      );
-      const json = await response.json();
-      likeCounter++;
-      //console.log("Likecounter: ", likeCounter);
-
-      totalLikes.innerHTML = "${likeCounter}";
-    } catch (e) {
-      console.log(e.message);
-    }
+    li.appendChild(figure);
+    ul.appendChild(li);
   });
 };
 // Move to login if not logged in
 loginBtn.addEventListener('click', () => {
   if (!user && !token) {
     location.href = '../html/login.html';
+    console.log('redirect to login');
   } else {
     loginBtn.style.display = 'none';
   }
 });
-
 // Move to profile
 profileBtn.addEventListener('click', () => {
   location.href = '../html/profile.html';
@@ -207,91 +69,15 @@ commentBtn.addEventListener('click', () => {
   console.log('redirect to comment');
 });
 
-//*AJAX to get the feedpost
-const getFeedPost = async () => {
+// AJAX
+const getPics = async () => {
   try {
-    const fetchOptions = {
-      headers: {
-        Authorization: "Bearer " + sessionStorage.getItem("token"),
-      },
-    };
-
-    const response = await fetch(url + "/post", fetchOptions);
-    const feedPost = await response.json();
-    //console.log(feedPost);
-    createFeedPost(feedPost);
-    searchOption(feedPost);
+    const response = await fetch(url + '/feed');
+    const pics = await response.json();
+    createFeedCards(pics);
   } catch (e) {
     console.log(e.message);
   }
 };
-getFeedPost();
 
-//* SEARCH Functionality
-
-const searchBtn = document.querySelector("#searchBtn");
-const searchItem = document.querySelector("#search-item");
-const searchResult = document.querySelector("#search-result");
-
-const createSearchCard = (searchPost) => {
-  searchResult.innerHTML += `
-     <div class="card">
-  <div class="cardTopDiv">
-    <img
-              src="https://place-puppy.com/400x400"
-              alt="Dog"
-              class="cardProfileImg"
-            />
-            <div class="text-container">
-            <h3>${searchPost.profilename}</h3>
-            <p class="location">${searchPost.location}</p>
-          </div>
-
-  </div>
-  <img
-      src="${url + "/thumbnails/" + searchPost.filename}"
-      alt="${searchPost.profilename}'s post"
-      style="width: 100%"
-    />
-  <div class="container">
-    <div class="likes-comments">
-      <i id="like-btn" class="fa-regular fa-heart"></i>
-      <i id="comment-btn" class="fa-regular fa-comment"></i>
-    </div>
-    <p class="likes">${searchPost.like_num}</p>
-    <p class="description">${searchPost.description}</p>
-    
-    </div>
-    </div>`;
-};
-const searchOption = (feedSearch) => {
-  //*Function to handle the keyboard press
-
-  searchItem.addEventListener("keyup", (e) => {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      searchBtn.click();
-    }
-  });
-
-  searchBtn.addEventListener("click", () => {
-    searchResult.innerHTML = "";
-    const userInput = searchItem.value;
-    console.log(userInput);
-    let array = [];
-
-    for (let i = 0; i < feedSearch.length; i++) {
-      const header = feedSearch[i].profilename.toLowerCase();
-      console.log(header);
-
-      if (header.includes(userInput)) {
-        console.log("Header includes userinput");
-        createSearchCard(feedSearch[i]);
-        feed.innerHTML = "";
-        array.push(feedSearch[i]);
-      }
-    }
-    //Emptying the search field
-    searchItem.value = "";
-  });
-};
+getPics();
--- frontend/js/post.js
+++ frontend/js/post.js
@@ -1,33 +1,24 @@
-"use strict";
-const url = "http://localhost:3000";
+'use strict';
 
-const addForm = document.querySelector("#addFormPost");
+const homeBtn = document.querySelector('.fa-solid.fa-house');
+const profileBtn = document.querySelector('.fa-solid.fa-user');
 
-addForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const formData = new FormData(addForm);
-
-  if (!sessionStorage.getItem("token") && !sessionStorage.getItem("user")) {
-    alert("Please login to post");
-    location.href = "../html/login.html";
-  }
-
-  const fetchOptions = {
-    method: "POST",
-    headers: {
-      Authorization: "Bearer " + sessionStorage.getItem("token"),
-    },
-    body: formData,
+const loadFile = function(event) {
+  const output = document.getElementById('output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src); // free memory
   };
+};
 
-  const response = await fetch(url + "/post", fetchOptions);
-  const json = await response.json();
+// Move to home
+homeBtn.addEventListener('click', () => {
+  location.href = 'feed.html';
+  console.log('redirect to home');
+});
 
-  console.log("Fetching the json:", json);
-  if (!response.ok) {
-    alert(json.message);
-    return;
-  }
-  alert("Your post is created");
-  location.href = "../html/feed.html";
+// Move to profile
+profileBtn.addEventListener('click', () => {
+  location.href = 'profile.html';
+  console.log('redirect to feed');
 });
